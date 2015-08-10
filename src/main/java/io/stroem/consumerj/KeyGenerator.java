842e80b23045d00dd8d8c36983a8dd12a4aad55d
ollekullberg <olle.kullberg@gmail.com>
2015-08-10 12:04:48 +0200

    Adding key generation functionality.

---
+++ src/main/java/io/stroem/consumerj/KeyGenerator.java
@@ -0,0 +1,25 @@
+package io.stroem.consumerj;
+
+import io.stroem.javaapi.JavaToScalaBridge;
+import io.stroem.promissorynote.DigitalSignatureScheme;
+import org.bitcoinj.core.ECKey;
+import org.spongycastle.math.ec.ECPoint;
+
+import java.math.BigInteger;
+
+/**
+ * Can help you get a key with a private part (say if you run a HD wallet).
+ */
+public class KeyGenerator {
+
+   /**
+    * @return an ECKey with a private part. This can be usseful since a deterministic key does not have private part.
+    */
+   public static ECKey generateKeyWithPrivatePart() {
+
+      DigitalSignatureScheme.KeyPair keyPair = JavaToScalaBridge.generateKeyPair();
+      BigInteger priv = (BigInteger) keyPair.privateKey();
+      ECPoint pub = (ECPoint) keyPair.publicKey();
+      return ECKey.fromPrivateAndPrecalculatedPublic(priv, pub);
+   }
+}
